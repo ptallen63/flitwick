e3dd9667a459a0ef4e570b41ad9c661592efb5c6
Paul Allen <ptallen63@gmail.com>
2017-03-21 09:13:14 -0400

    adding commander

--- bin/flitwick.js
+++ bin/flitwick.js
@@ -2,21 +2,13 @@
 /* eslint-disable no-console */
 const flitwick = require('../dist/index').default;
 const pkg = require('../package.json');
+const program = require('commander');
 
-const args = process.argv;
-args.map((a) => {
-  if (a === '-v' || a === '-version') {
-    console.log(pkg.version);
-    process.exit();
-  }
-
-  if (a === '-help' || a === '-h') {
-    console.log('To run: flitwick <path to match list> <path to list to match with> <field to match on>');
-    console.log('please refer to https://github.com/ptallen63/flitwick');
-    process.exit();
-  }
-  return true;
-});
+program
+  .usage('flitwick <path/to/match_list> <path/to/match_to_list> <match_field>')
+  .version(pkg.version)
+  .description('put in the path to the list you want to match\n and the path to the list you are matching too.\n You must also put in the field you want to match on.')
+  .parse(process.argv);
 
 
-flitwick(args[2], args[3], args[4]);
+flitwick(program.args[0], program.args[1], program.args[2]);
