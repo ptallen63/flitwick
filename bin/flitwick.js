f8212de68d03f17e9e1148cc39f4e40561b6b55a
Paul Allen <ptallen63@gmail.com>
2017-03-21 11:42:47 -0400

    new build

--- bin/flitwick.js
+++ bin/flitwick.js
@@ -4,11 +4,34 @@ const flitwick = require('../dist/index').default;
 const pkg = require('../package.json');
 const program = require('commander');
 
+let matchListPath = null;
+let matchToListPath = null;
+let matchField = null;
+
 program
   .usage('flitwick <path/to/match_list> <path/to/match_to_list> <match_field>')
+  .arguments('<matchList> <matchTolist> <match>')
+  .action((matchList, matchTolist, match) => {
+    matchListPath = matchList;
+    matchToListPath = matchTolist;
+    matchField = match;
+  })
   .version(pkg.version)
   .description('put in the path to the list you want to match\n and the path to the list you are matching too.\n You must also put in the field you want to match on.')
   .parse(process.argv);
 
 
-flitwick(program.args[0], program.args[1], program.args[2]);
+if (!matchListPath) {
+  console.log('no match list provided');
+  process.exit(1);
+}
+if (!matchToListPath) {
+  console.log('no match to list provided');
+  process.exit(1);
+}
+if (!matchField) {
+  console.log('no match field provided');
+  process.exit(1);
+}
+
+flitwick(matchListPath, matchToListPath, matchField);
